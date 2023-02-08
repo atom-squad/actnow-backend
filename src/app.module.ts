73e50e0904927a2f56bdfd01a8441198b560faf9
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2023-02-07 16:05:27 -0800

    CAR-68 Generate User Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { QuestionnaireModule } from './questionnaire/questionnaire.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI),
     QuestionnaireModule,
+    UserModule,
   ],
 })
 export class AppModule {}
