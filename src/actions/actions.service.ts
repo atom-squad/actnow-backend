b0b931d3fea32c27218a44d075f0884bb389191a
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2023-02-19 22:11:53 -0800

    CAR-101 CAR-121: Create getActionById Method

--- src/actions/actions.service.ts
+++ src/actions/actions.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Action, ActionDocument } from 'src/schemas/action.schema';
 
 @Injectable()
-export class ActionsService {}
+export class ActionsService {
+
+    constructor(@InjectModel(Action.name) private actionModel: Model<ActionDocument>) {}
+
+    async getActionById(_id: string){
+        return this.actionModel.findById({
+            _id
+        }).exec()
+    }
+}
